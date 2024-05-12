9842dc036df7cd7ed892a2f4b6084d1d6a818f93
Jiro Shimaya <shimajiroxyz@users.noreply.github.com>
2024-05-13 00:08:13 +0900

    a

--- tests/test_phrase_splitter.py
+++ tests/test_phrase_splitter.py
@@ -10,10 +10,10 @@ class TestPhraseSplitter(unittest.TestCase):
             , ["勉強する", ["勉強する"]] #サ変接続名詞+サ変
             , ["abcする", ["abc", "する"]] #Unknown名詞+サ変
             , ["違うする", ["違う", "する"]] #名詞以外+サ変
-            , ["「あなたは、『どこ』？」", ["「あなたは、『", "どこ』？」"]] # 記号
+            , ["「あなたは、『どこ』？」", ["「あなたは、", "『どこ』？」"]] # 記号
             , ["はが", ["はが"]] # 助詞から始まる文節
             , ["て、に、を、はなどの", ["て、","に、","を、","はなどの"]] # 記号直後の助詞
-            , ["「て。」や「に」などの", ["「て。」や「","に」などの"]] # 閉じカッコ直後の助詞
+            , ["「て。」や「に」などの", ["「て。」や","「に」などの"]] # 閉じカッコ直後の助詞
         ]
 
     def test_split_text_surface_true(self):
@@ -24,7 +24,7 @@ class TestPhraseSplitter(unittest.TestCase):
             , ["寝ている」ところ", ["寝て","いる」", "ところ"]] # 非自立名詞
             ]
         for input_string, expected in self.base_cases + specific_cases:
-            result = self.splitter.split_text(input_string, output_type=PhraseSplitter.OUTPUT_SURFACE, consider_non_independent_nouns_as_breaks=True)
+            result = self.splitter.split_text(input_string, output_type=PhraseSplitter.OUTPUT_SURFACE, consider_non_independent_nouns_and_verbs_as_breaks=True)
             self.assertEqual(result, expected)
 
     def test_split_text_surface_false(self):
@@ -34,7 +34,7 @@ class TestPhraseSplitter(unittest.TestCase):
             , ["寝ている」ところ", ["寝ている」ところ"]] # 非自立名詞
             ]
         for input_string, expected in self.base_cases + specific_cases:
-            result = self.splitter.split_text(input_string, output_type=PhraseSplitter.OUTPUT_SURFACE, consider_non_independent_nouns_as_breaks=False)
+            result = self.splitter.split_text(input_string, output_type=PhraseSplitter.OUTPUT_SURFACE, consider_non_independent_nouns_and_verbs_as_breaks=False)
             self.assertEqual(result, expected)
 
     def test_split_text_detailed_true(self):
@@ -42,7 +42,7 @@ class TestPhraseSplitter(unittest.TestCase):
             ["寝ている", [['寝', 'て'], ['いる']]]
             ]
         for input_string, expected in specific_cases:
-            detailed_phrases = self.splitter.split_text(input_string, PhraseSplitter.OUTPUT_DETAILED, consider_non_independent_nouns_as_breaks=True)
+            detailed_phrases = self.splitter.split_text(input_string, PhraseSplitter.OUTPUT_DETAILED, consider_non_independent_nouns_and_verbs_as_breaks=True)
             surface_forms_only = [[token['surface_form'] for token in phrase] for phrase in detailed_phrases]
             result = surface_forms_only
             self.assertEqual(result, expected)
@@ -52,7 +52,7 @@ class TestPhraseSplitter(unittest.TestCase):
             ["今日は", [{'surface_form': '今日は', 'reading': 'キョウハ', 'pronunciation': 'キョーワ'}]]
             ]
         for input_string, expected in specific_cases:
-            result = self.splitter.split_text(input_string, output_type=PhraseSplitter.OUTPUT_CONCATENATED, consider_non_independent_nouns_as_breaks=True)
+            result = self.splitter.split_text(input_string, output_type=PhraseSplitter.OUTPUT_CONCATENATED, consider_non_independent_nouns_and_verbs_as_breaks=True)
             self.assertEqual(result, expected)
 
 
